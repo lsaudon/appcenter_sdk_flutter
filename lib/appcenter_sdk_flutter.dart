5f7d69e91fded35334580d41034ece9cea8c90df
Lucas SAUDON <lsaudon@gmail.com>
2024-10-11 22:22:18 +0200

    feat(linter): add new linting rules
    Added several new linting rules to improve code quality and maintainability, including: ...
    
    refactor(Messages.g.kt): update autogenerated comment and method signature

--- lib/appcenter_sdk_flutter.dart
+++ lib/appcenter_sdk_flutter.dart
@@ -1,6 +1,3 @@
-/// Development plugin for the App Center Analytics and Crashes for Flutter.
-library appcenter_sdk_flutter;
-
 export 'src/appcenter/appcenter.dart';
 export 'src/appcenter_analytics/appcenter_analytics.dart';
 export 'src/appcenter_crashes/appcenter_crashes.dart';
